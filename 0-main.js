42b7acdab7da8f94a4974a30dab8013891c45f8b
Amazingmaroke <marokemaroke2020@gmail.com>
2023-07-25 19:49:34 +0100

    all

--- 0-main.js
+++ 0-main.js
@@ -1,3 +1,8 @@
-import getSanFranciscoDescription from './0x00-ES6_basic/6-string-interpolation';
+import getFullResponseFromAPI from './0x01-ES6_promise/1-promise';
 
-console.log(getSanFranciscoDescription());
+console.log(`This is the resolved object ${getFullResponseFromAPI(true)}`);
+const result = getFullResponseFromAPI(true).then((data) => {
+  console.log(data.status);
+});
+console.log(result); // this will show promise status
+// console.log(getFullResponseFromAPI(false));
